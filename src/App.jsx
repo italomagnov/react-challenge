b17744f913a95a4e0cec75a425b12ee86264cd58
italomagnov <italomagnov@gmail.com>
2022-12-31 13:44:26 -0400

    Desafio React FrontEnd

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+    const [clickList, setClickList] = useState([]);
+    const [undone, setUndone] = useState([]);
+
+    const handleClick = (event) => {
+        const newDot = {
+            coordX: event.clientX,
+            coordY: event.clientY,
+        };
+
+        setClickList((coord) => [...coord, newDot]);
+    };
+
+    const handleUndo = (event) => {
+        event.stopPropagation();
+
+        if (clickList.length === 0) {
+            return;
+        }
+
+        const lastItem = clickList[clickList.length - 1];
+        setUndone((prev) => [...prev, lastItem]);
+
+        setClickList((prev) => {
+            const newArray = [...prev].slice(0, -1);
+            return newArray;
+        });
+    };
+
+    const handleRedo = (event) => {
+        event.stopPropagation();
+
+        if (undone.length === 0) {
+            return;
+        }
+
+        const dot = undone[undone.length - 1];
+
+        setUndone((prev) => {
+            const newArray = [...prev].slice(0, -1);
+            return newArray;
+        });
+        setClickList((prev) => [...prev, dot]);
+    };
+
+    return (
+        <div className="App" onClick={handleClick}>
+            <h1>React Challenge</h1>
+            <div className="buttons">
+                <button onClick={handleUndo}>Desfazer</button>
+                <button onClick={handleRedo}>Refazer</button>
+            </div>
+            {clickList.map((item) => (
+                <div
+                    className="circle"
+                    style={{ left: item.coordX - 25, top: item.coordY - 25 }}
+                ></div>
+            ))}
+        </div>
+    );
+}
+
+export default App;
